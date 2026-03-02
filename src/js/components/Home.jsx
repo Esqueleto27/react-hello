949a11031673da871424bfdb6f901f2eb87552dd
EmanuelMejia <emanuel@firedrop.mx>
2026-03-02 17:23:23 -0600

    agregar cards y footer

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,51 @@
 import React from "react";
 
-//include images into your bundle
+//imports
 import rigoImage from "../../img/rigo-baby.jpg";
+import cardImage from "../../img/project-image.png";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import SingleCard from "./card";
+import Footer from "./footer";
+
+const defCardTitle = "Card Title";
+const defCardText =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tempora cupiditate optio asperiores cumque voluptatibus, quidem illo? Aliquam sapiente, eveniet, consequuntur explicabo cumque omnis architecto quidem iste dolor, tempora optio!";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  return (
+    <div className="text-center">
+      <Navbar />
+      <div class="row mx-4">
+        <Jumbotron />
+      </div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+      <div class="row mx-4">
+        <SingleCard
+          title={defCardTitle}
+          text={defCardText}
+          imageURL={cardImage}
+        />
+        <SingleCard
+          title={defCardTitle}
+          text={defCardText}
+          imageURL={cardImage}
+        />
+        <SingleCard
+          title={defCardTitle}
+          text={defCardText}
+          imageURL={cardImage}
+        />
+        <SingleCard
+          title={defCardTitle}
+          text={defCardText}
+          imageURL={cardImage}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
