3b4fb50621abda88186ad0124ca8383c42c6e64b
Matheo Flores Loor <matheofloresloor@gmail.com>
2026-03-02 18:51:26 -0500

    Arreglo del Navbar

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -2,37 +2,44 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <a className="navbar-brand" href="#">
-          Mi Sitio
+          Start Bootstrap
         </a>
+
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Inicio <span className="sr-only">(current)</span>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Home
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Acerca de
+                About
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Contacto
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
               </a>
             </li>
           </ul>
