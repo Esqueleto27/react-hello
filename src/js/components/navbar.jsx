7e6a9061a41165b3d0f8d9d7b31f8ad5dab637d2
Matheo Flores Loor <matheofloresloor@gmail.com>
2026-03-02 19:03:54 -0500

    Arreglo del bug del navbar

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -13,9 +13,6 @@ const Navbar = () => {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
